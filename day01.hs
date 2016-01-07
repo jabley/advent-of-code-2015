e52a1fb2032f81d2bf95b75363245071c5126e39
James Abley <james.abley@gmail.com>
2016-01-07 19:51:13 +0000

    Do part 2 of day 1
    Missed this off.

--- day01.hs
+++ day01.hs
@@ -6,4 +6,11 @@ solve n [] = n
 solve n ('(' : xs) = solve (n + 1) xs
 solve n (')' : xs) = solve (n - 1) xs
 
-main = puzzle >>= print . solve 0
+solve' (-1) i _ = i
+solve' floor i ('(' : xs) = solve' (floor+1) (i+1) xs
+solve' floor i (')' : xs) = solve' (floor-1) (i+1) xs
+
+main = do
+    p <- puzzle
+    print $ solve 0 p
+    print $ solve' 0 0 p
