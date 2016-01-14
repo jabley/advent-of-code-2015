50ff2422923048c5977abf7b508882f5054e03df
James Abley <james.abley@gmail.com>
2016-01-14 07:50:19 +0000

    Do part 2 of day 3 puzzle

--- day03.hs
+++ day03.hs
@@ -11,4 +11,15 @@ visited (x, y) history ('v' : xs) = visited (x, y-1) ((x, y) : history) xs
 visited (x, y) history ('>' : xs) = visited (x+1, y) ((x, y) : history) xs
 visited (x, y) history ('<' : xs) = visited (x-1, y) ((x, y) : history) xs
 
-main = puzzle >>= print . length . visited (0,0) []
+santa p = visited (0,0) [] p
+
+robot p = visited (0,0) [] p
+
+everyOther [] = []
+everyOther [x] = [x]
+everyOther (x : _ : xs) = x : everyOther xs
+
+main = do
+    p <- puzzle
+    print $ length $ (visited (0,0) [] p)
+    print $ length $ unions [santa (everyOther p), robot (everyOther (tail p))]
