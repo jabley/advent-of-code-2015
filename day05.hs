ee5d94771759474b84fb7be9ab986faff2aa5bcd
James Abley <james.abley@gmail.com>
2016-02-17 19:52:00 +0000

    Do part 2 of day 5 puzzle
    Took ages to name the match :D

--- day05.hs
+++ day05.hs
@@ -23,9 +23,22 @@ noBanned = not . any (`elem` ["ab", "cd", "pq", "xy"]) . pairs
 
 (&&&) a b = (&&) <$> a <*> b
 
+nonAdjacentDoubled [] = False
+nonAdjacentDoubled [_] = False
+nonAdjacentDoubled (x:xs) = (x `elem` tail xs) || nonAdjacentDoubled xs
+
+doubledPair = nonAdjacentDoubled . pairs
+
+separated [_] = False
+separated [_, _] = False
+separated (x:y:xs) = x == head xs || separated (y:xs)
+
 nice = threeOrMoreVowels &&& atLeastOneDouble &&& noBanned
+
+improvedNice = doubledPair &&& separated
 
 main = do
     p <- puzzle
     print $ length $ filter nice p
+    print $ length $ filter improvedNice p
 
